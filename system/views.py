db762c9bed5ede3fb3f5ba2a1d007d72194b8b96
labtek <labtek@labtek009-O-E-M.(none)>
2011-03-26 11:54:33 +0700

    masukin theme system

--- system/views.py
+++ system/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse('ini nanti nya admin')
+	return render_to_response('system/dashboard.html')
