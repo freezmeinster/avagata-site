58b659166dea1631b1f987160137eb479d8deed6
bram <freez_meinster@yahoo.co.id>
2011-03-26 15:40:24 +0700

    crud buat kategori

--- system/urls.py
+++ system/urls.py
@@ -4,6 +4,11 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('system.views',
 	url(r'^$', 'index', name='system_index'),
+	
 	url(r'^post$', 'post', name='post_index'),
+	
+	
 	url(r'^kategori$', 'kategori', name='kategori_index'),
+	url(r'^kategori/(?P<kategori_id>\d+)/edit/?$', 'kategori_edit', name='kategori_edit'),
+	url(r'^kategori/(?P<kategori_id>\d+)/hapus/?$', 'kategori_hapus', name='kategori_hapus'),
 )
--- system/views.py
+++ system/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,redirect
 from django.template import RequestContext
 from blog.models import Post,Kategori,Komentar
 from blog.forms import KategoriForm
@@ -7,11 +7,16 @@ def index(request):
 	
 	return render_to_response('system/dashboard.html')
 
+############### View Buat Post  #######################
 def post(request):
 	
 	return render_to_response('system/dashboard.html',{
 		'url'  :  'post',
 	})
+
+#######################################################
+
+################ View buat Kategori ###################
 
 def kategori(request):
 	kategori = Kategori.objects.all()
@@ -30,3 +35,27 @@ def kategori(request):
 		'form' 		:  form,
 		'success'	:  success,
 	},context_instance=RequestContext(request))
+
+def kategori_edit(request,kategori_id):
+	kat = Kategori.objects.get(id=kategori_id)
+	katform = KategoriForm(instance=kat)
+	success = False
+	
+	if request.method  == 'POST' :
+		katform = KategoriForm(request.POST,instance=kat)
+		if katform.is_valid:
+			katform.save()
+			success = True
+			
+	return render_to_response('system/kategori/edit.html',{
+		'url' 		: 'kategori',
+		'kategori' 	: katform,
+		'kat' 		: kat,
+		'success' 	: success ,
+	},context_instance=RequestContext(request))
+	
+def kategori_hapus(request,kategori_id):
+	kat = Kategori.objects.get(id=kategori_id)
+	kat.delete()
+	return redirect('kategori_index')
+####################################################
